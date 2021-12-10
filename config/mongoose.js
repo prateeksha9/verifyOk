3ea45c3b856d244de7686a12f6f3549be5988856
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-10 12:18:17 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 // const env = require("./enviorment");
-mongoose.connect(`mongodb://localhost/${process.env.db}`);
+
+mongoose.connect(mongodb+srv://Prateeksha:prateeksha@0209@cluster0.i1mu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority)
+//   `mongodb://localhost/${process.env.db}`);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error Connecting to MongoDB"));
