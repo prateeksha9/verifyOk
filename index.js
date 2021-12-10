8f094f299ddcdeac9241abde307b7548ccd4f950
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-10 12:31:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT;
 // || 8000;
 const expressLayouts = require("express-ejs-layouts");
 const passport = require("passport");
